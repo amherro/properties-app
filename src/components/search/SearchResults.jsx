86bd66dd9009e85bd3cd26d569dec46c36b22356
Adam Herro <adam.herro@comcast.net>
2025-07-24 17:31:16 -0400

    Implementing fetch request on the search box.

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react"
-import Searchbox from "./Searchbox"
+import { useEffect, useState } from 'react';
+import Searchbox from './Searchbox';
 
 const SearchResults = () => {
-  const [searchResults, setSearchResults] = useState([])
-  const searchPosts = (result) => {
-    setSearchResults((searchResults) => [...searchResults, result])
-  }
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <>
-      <Searchbox searchPosts={searchPosts} searchResults={searchResults} />
+      <Searchbox searchResults={searchResults} />
       <div className="results-section">
         <h1>Results</h1>
         {searchResults.map((result, index) => (
-            <h2 key={index}>{result.search}</h2>
-          )
-        )}
+          <h2 key={index}>{result.search}</h2>
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SearchResults
+export default SearchResults;
--- src/components/search/Searchbox.jsx
+++ src/components/search/Searchbox.jsx
@@ -1,46 +1,65 @@
-import { ToastContainer, toast } from "react-toastify"
+import { ToastContainer, toast } from 'react-toastify';
 
-const Searchbox = ({searchPosts}) => {
-    const submitSearch = (formData) => {
-
-        const formSearch = {
-            search: formData.get("search")
-        }
-        if(formSearch.search === '') {
-            return(
-                toast("Please type something...", {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    progress: undefined,
-                    className: "error-toast"
-                })
-            )
-        }
-        return(
-            searchPosts(formSearch)
-        )
+// const Searchbox = ({searchPosts}) => {
+const Searchbox = ({}) => {
+  const searchPosts = async () => {
+    // const url = process.env.API_URL
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    try {
+      const res = await fetch(url);
+      if (res.error) {
+        throw new Error(`Error: ${res.status}`);
+      }
+      const data = await res.json();
+      // setSearchResults(data => [...searchResults, data.body])
+      console.log(data);
+      return data.body;
+    } catch (error) {
+      throw new Error(`Error: ${error}`);
     }
-    
+  };
+
+  const submitSearch = (formData) => {
+    const formSearch = {
+      // search: formData.get("search")
+      search: searchPosts(),
+    };
+    if (formSearch.search === '') {
+      return toast('Please type something...', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        className: 'error-toast',
+      });
+    }
+    return searchPosts(formSearch);
+  };
+
   return (
     <div className="searchbox">
-        <form action={submitSearch} className='search-form'>
-            <label htmlFor="search" className="search-title">Find Properties</label>
-            <input
-                type="text"
-                id="search"
-                name="search"
-                placeholder="Search..."
-                className="search-input"
-            />
-            <button type="submit" className="search-button">Search</button>
-        </form>
-        <ToastContainer />
+      <form action={submitSearch} className="search-form">
+        <label htmlFor="search" className="search-title">
+          Find Properties
+        </label>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Search..."
+          className="search-input"
+          maxLength={50}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Searchbox
+export default Searchbox;
