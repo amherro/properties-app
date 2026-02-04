92d3b604a253e803ca6aaa062ab18ed9f97b15fd
Adam Herro <adam.herro@comcast.net>
2026-02-03 19:06:33 -0500

    Adding footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Searchbox from './components/search/Searchbox';
 // import SearchResults from './components/search/SearchResults';
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Searchbox />
       </main>
+      <Footer />
     </>
   );
 }
