7458071d9d56c70839420314dd6fae1149ee37dd
Adam Herro <adam.herro@comcast.net>
2026-02-17 15:44:35 -0500

    Adding more data to character cards. Styling search area.

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 const CharacterCard = ({ data, house }) => {
   return (
     <div className={`card ${house ? house : 'noHouseListed'}`}>
+      {data.image && <img src={data.image} alt="" className="characterImg" />}
       <h3 className="name">{data.name}</h3>
+
       <p className="house">House: {data.house ? data.house : 'N/A'}</p>
       <p className="dob">DOB: {data.dateOfBirth ? data.dateOfBirth : 'N/A'}</p>
       <p className="patronus">
         {data.patronus.toUpperCase() && `Patronus: ${data.patronus}`}
+      </p>
+      <p className="ancestry">
+        {data.ancestry && `Ancestry: ${data.ancestry}`}
       </p>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="header-container">
       <h1 className="logo">
-        <a href="/">HomeSteady</a>
+        <a href="/">Wizard World Reference</a>
       </h1>
       <nav>
         <ul className="nav-links-container">
@@ -13,9 +13,6 @@ const Header = () => {
           </li>
           <li className="nav-link">
             <NavLink to="/about">About</NavLink>
-          </li>
-          <li className="nav-link">
-            <NavLink to="/contact">Contact</NavLink>
           </li>
           <li className="nav-link">
             <NavLink to="/spells">Spells</NavLink>
