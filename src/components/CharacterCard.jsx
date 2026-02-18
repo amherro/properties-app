9f83e8aafd4a9c371f4ce9cdf991ba4b9baeb96c
Adam Herro <adam.herro@comcast.net>
2026-02-17 20:10:36 -0500

    Creating modal for more character info

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
+import CharacterModal from './CharacterModal';
 
 const CharacterCard = ({ data, house }) => {
   return (
     <div className={`card ${house ? house : 'noHouseListed'}`}>
       {data.image && <img src={data.image} alt="" className="characterImg" />}
       <h3 className="name">{data.name}</h3>
-
-      <p className="house">House: {data.house ? data.house : 'N/A'}</p>
-      <p className="dob">DOB: {data.dateOfBirth ? data.dateOfBirth : 'N/A'}</p>
-      <p className="patronus">
-        {data.patronus.toUpperCase() && `Patronus: ${data.patronus}`}
-      </p>
-      <p className="ancestry">
-        {data.ancestry && `Ancestry: ${data.ancestry}`}
-      </p>
+      <dialog>
+        <CharacterModal data={data} />
+      </dialog>
+      <button className="more-info">More Information</button>
     </div>
   );
 };
