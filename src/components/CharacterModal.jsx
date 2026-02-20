3b1ad4c990f0b0b0a2fb32a18e0d19215d6d8208
Adam Herro <adam.herro@comcast.net>
2026-02-20 14:56:21 -0500

    Styling character modals

--- src/components/CharacterModal.jsx
+++ src/components/CharacterModal.jsx
@@ -15,19 +15,33 @@ const CharacterModal = ({ modalData, closeModal }) => {
       >
         <Icon path={mdiCloseCircleOutline} size={1.4} />
       </button>
-      <h3 className="name">{modalData.name}</h3>
-      <p className="house">
-        House: {modalData.house ? modalData.house : 'N/A'}
-      </p>
-      <p className="dob">
-        DOB: {modalData.dateOfBirth ? modalData.dateOfBirth : 'N/A'}
-      </p>
-      <p className="patronus">
-        {modalData.patronus && `Patronus: ${modalData.patronus}`}
-      </p>
-      <p className="ancestry">
-        {modalData.ancestry && `Ancestry: ${modalData.ancestry}`}
-      </p>
+      <span className="modal-data">
+        <img
+          src={modalData.image}
+          alt={`Image of ${modalData.name}`}
+          className="characterImg"
+        />
+        <h3 className="name">{modalData.name}</h3>
+        <p className="dob">
+          DOB: {modalData.dateOfBirth ? modalData.dateOfBirth : 'N/A'}
+        </p>
+        <p className="gender">Gender: {modalData.gender}</p>
+        <p className="house">
+          House: {modalData.house ? modalData.house : 'N/A'}
+        </p>
+        <p className="patronus">
+          {modalData.patronus && `Patronus: ${modalData.patronus}`}
+        </p>
+        <p className="ancestry">
+          {modalData.ancestry && `Ancestry: ${modalData.ancestry}`}
+        </p>
+        <p className="status">
+          {`Status: ${modalData.alive ? 'Alive' : 'Deceased'}`}
+        </p>
+        <p className="actor">
+          {modalData.actor && `Actor: ${modalData.actor}`}
+        </p>
+      </span>
     </dialog>
   );
 };
