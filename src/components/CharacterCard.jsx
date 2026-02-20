bb26e110a0c8198f94b57fcc5d3b1da367677958
Adam Herro <adam.herro@comcast.net>
2026-02-20 11:40:09 -0500

    Fixing modal so correct data is shown (had to set isVisible back to false). Initial modal styling.

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 import CharacterModal from './CharacterModal';
 
 const CharacterCard = ({ data, house }) => {
+  const [modalData, setModalData] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const getModalData = async (data) => {
+    setModalData(data);
+    setIsVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsVisible(false);
+  };
+
   return (
     <div className={`card ${house ? house : 'noHouseListed'}`}>
       {data.image && <img src={data.image} alt="" className="characterImg" />}
       <h3 className="name">{data.name}</h3>
-      <dialog>
-        <CharacterModal data={data} />
-      </dialog>
-      <button className="more-info">More Information</button>
+      {isVisible && (
+        <CharacterModal modalData={modalData} closeModal={closeModal} />
+      )}
+      <button
+        onClick={() => getModalData(data)}
+        command="show-modal"
+        commandfor="character-modal"
+        className="more-info"
+      >
+        More Information
+      </button>
     </div>
   );
 };
--- src/components/CharacterModal.jsx
+++ src/components/CharacterModal.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-
-const CharacterModal = ({ data }) => {
+import Icon from '@mdi/react';
+import { mdiCloseCircleOutline } from '@mdi/js';
+const CharacterModal = ({ modalData, closeModal }) => {
   return (
-    <>
-      <h3 className="name">{data.name}</h3>
-      <p className="house">House: {data.house ? data.house : 'N/A'}</p>
-      <p className="dob">DOB: {data.dateOfBirth ? data.dateOfBirth : 'N/A'}</p>
+    <dialog
+      id="character-modal"
+      className={`${modalData.house.toLowerCase() ? modalData.house.toLowerCase() : 'noHouseListed'}`}
+      closedby="close"
+    >
+      <button
+        className="modal-close"
+        commandfor="character-modal"
+        command="close"
+        onClick={() => closeModal()}
+      >
+        <Icon path={mdiCloseCircleOutline} size={1.4} />
+      </button>
+      <h3 className="name">{modalData.name}</h3>
+      <p className="house">
+        House: {modalData.house ? modalData.house : 'N/A'}
+      </p>
+      <p className="dob">
+        DOB: {modalData.dateOfBirth ? modalData.dateOfBirth : 'N/A'}
+      </p>
       <p className="patronus">
-        {data.patronus.toUpperCase() && `Patronus: ${data.patronus}`}
+        {modalData.patronus && `Patronus: ${modalData.patronus}`}
       </p>
       <p className="ancestry">
-        {data.ancestry && `Ancestry: ${data.ancestry}`}
+        {modalData.ancestry && `Ancestry: ${modalData.ancestry}`}
       </p>
-    </>
+    </dialog>
   );
 };
 
